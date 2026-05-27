5c492759eb7a6bbdd8d069eebc28f4f875a1661f
Ben Ashby <mail@benashby.com>
2026-05-27 09:41:10 -0600

    add nix package derivation

---
+++ nix/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenvNoCC,
+  makeWrapper,
+  bash,
+  coreutils,
+  jq,
+  socat,
+  hyprland ? null,
+  withHyprland ? true,
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "hyprplane";
+  version = "0.1.0";
+
+  src = ../.;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  postInstall = ''
+    wrapProgram $out/bin/hyprplane \
+      --prefix PATH ':' "${lib.makeBinPath (
+        [ bash coreutils jq socat ]
+        ++ lib.optional withHyprland hyprland
+      )}"
+    wrapProgram $out/bin/hyprplane-status \
+      --prefix PATH ':' "${lib.makeBinPath [ coreutils jq ]}"
+  '';
+
+  meta = with lib; {
+    description = "Plane-level workspace grouping for Hyprland";
+    homepage    = "https://github.com/benashby/hyprplane";
+    license     = licenses.mit;
+    platforms   = platforms.linux;
+    mainProgram = "hyprplane";
+  };
+}
